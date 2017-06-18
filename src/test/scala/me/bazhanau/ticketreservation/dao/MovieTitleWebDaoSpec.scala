1a1d5b5c5257b519e0b4975925c305374de569e0
Aliaksandr Bazhanau <a.bazhanau@adform.com>
2017-06-18 23:26:23 +0300

    configuration added into MovieTitleWebDaoSpec

--- src/test/scala/me/bazhanau/ticketreservation/dao/MovieTitleWebDaoSpec.scala
+++ src/test/scala/me/bazhanau/ticketreservation/dao/MovieTitleWebDaoSpec.scala
@@ -6,6 +6,7 @@ import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.model.Uri
 import akka.stream.ActorMaterializer
 import akka.testkit.TestKit
+import com.typesafe.config.ConfigFactory
 import org.scalatest._
 
 import scala.util.Random
@@ -15,9 +16,10 @@ class MovieTitleWebDaoSpec extends TestKit(ActorSystem("MovieTitleWebDaoSpec"))
   with AsyncFlatSpecLike {
 
   implicit val materializer = ActorMaterializer()
+  val config = ConfigFactory.load()
   val f = Http().singleRequest(_ : HttpRequest)
-  val baseUri = Uri("http://www.omdbapi.com/")
-  val dao: MovieTitleDao = new MovieTitleWebDao(f, baseUri, "289ba303")
+  val baseUri = Uri(config.getString("omdbapi.baseUrl"))
+  val dao: MovieTitleDao = new MovieTitleWebDao(f, baseUri, config.getString("omdbapi.apiKey"))
 
   "MovieTitleWebDao" should "return title, if it exists" in {
     dao.find("tt0111161").map(s => {
